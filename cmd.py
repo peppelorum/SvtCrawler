a08822d9a924d88e1b189a9a6f3eb3f5bf9eb7e8
Peppe Bergqvist <p@bergqvi.st>
2013-04-11 12:24:35 +0200

    Major rewrite

--- cmd.py
+++ cmd.py
@@ -1,28 +1,36 @@
-__author__ = 'peppe'
-
-import os
-from optparse import OptionParser
+# -*- coding: utf-8 -*-
+#
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# Peppe Bergqvist <p@bergqvi.st> wrote this file. As long as you retain this
+# notice you can do whatever you want with this stuff. If we meet some day,
+# and you think this stuff is worth it, you can buy me a beer in return.
+# ----------------------------------------------------------------------------
+#
 
 from svtcrawler import SvtCrawler
 
-
 def main():
-    parser = OptionParser(usage="usage: %prog [options]")
-    parser.add_option("-f", "--filename",
-                      action="store", # optional because action defaults to "store"
-                      dest="filename",
-                      help="Filename for hit list",)
-    (options, args) = parser.parse_args()
 
-    if len(args) != 0:
-        parser.error("wrong number of arguments")
+    print '*********'
+    print 'CRAWLING!'
+    print '*********'
 
+    obj = SvtCrawler()
 
-    path = os.path.dirname(os.path.realpath(__file__))
-    filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), options.filename)
+    for a in obj.categories:
+        print a.title
+        for b in a.shows:
 
-    obj = SvtCrawler(filepath)
-    obj.crawl()
+            print '\t', b.title, b.url
+
+            print '\tEpisoder:'
+            for c in b.episodes:
+                print '\t\t', c.title
+
+            print '\tKlipp:'
+            for c in b.clips:
+                print '\t\t', c.title
 
 if __name__ == '__main__':
     main()
--- svtcrawler/__init__.py
+++ svtcrawler/__init__.py
@@ -2,37 +2,256 @@
 #
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE" (Revision 42):
-# <p@bergqvi.st> wrote this file. As long as you retain this notice you
-# can do whatever you want with this stuff. If we meet some day, and you think
-# this stuff is worth it, you can buy me a beer in return. Peppe Bergqvist
+# Peppe Bergqvist <p@bergqvi.st> wrote this file. As long as you retain this
+# notice you can do whatever you want with this stuff. If we meet some day,
+# and you think this stuff is worth it, you can buy me a beer in return.
 # ----------------------------------------------------------------------------
 #
 
+from datetime import datetime, timedelta
+from urllib2 import HTTPError
+from pytz import timezone, utc
+from pyquery import PyQuery
 
-import os
-import requests
-#from pyquery import PyQuery
-#import lxml.html
-from bs4 import BeautifulSoup
+TIME_ZONE = 'Europe/Stockholm'
 
 
-class SvtCrawler():
+def shellquote(s):
+    s = s.replace('/', '-')
+    valid_chars = u' -_.():0123456789abcdefghijklmnpoqrstuvwxyzåäöABCDEFGHIJKLMNOPQRSTUVZXYZÅÄÖ'
+    s = ''.join(c for c in s if c in valid_chars)
+    s = s.strip()
 
-    def __init__(self, filepath):
+    return s
 
-        self.searchPhrases  = [line.strip() for line in open(filepath)]
-        self.url = 'http://www.svtplay.se/program'
+
+def numerics(s):
+    n = 0
+    for c in s:
+        if not c.isdigit():
+            return n
+        else:
+            n = n * 10 + int(c)
+    return n
+
+
+def parse_date(datum, typ):
+    datum = unicode(datum)
+    tz = timezone(TIME_ZONE)
+    current_timezone = datetime.utcnow().replace(tzinfo=tz)
+    if datum.find('dag') != -1:
+        days = numerics(datum)
+        if typ == '+':
+            ret = current_timezone + timedelta(days=days)
+        else:
+            ret = current_timezone - timedelta(days=days)
+    elif datum.find('tim') != -1:
+        hours = numerics(datum)
+        if typ == '+':
+            ret = current_timezone + timedelta(hours=hours)
+        else:
+            ret = current_timezone - timedelta(hours=hours)
+    else:
+        return current_timezone
+
+    return ret
+
+
+def sanitize_description(value):
+    cleaned = PyQuery(value)
+    cleaned = cleaned.remove('span.playMetaText')
+    cleaned.remove('span.playMetaText')
+    cleaned.remove('time')
+    cleaned.remove('strong')
+
+    return cleaned.html().split('<span>')[-1:][0].replace('</span>', '')
+
+class Category:
+    def __init__(self, title, url, html_class, thumbnail):
+        self.title = title
+        self.url = url
+        self.html_class = html_class
+        self.thumbnail = thumbnail
+
+
+class Show:
+    def __init__(self, title, url, thumbnail):
+        self.title = title
+        self.url = url
+        self.thumbnail = thumbnail
+
+
+class Episode:
+    pass
+
+
+class Episodes:
+    def __init__(self, crawler, url, kind_of):
+        self.crawler = crawler
+        self.i = 0
+        self.kind_of = kind_of
+
+        self.episodes = PyQuery(url)
+        if kind_of == 'episodes':
+            self.episodes_iter = self.episodes.find('.svtTab-1.playBoxBody').find('article.svtUnit')
+        else:
+            self.episodes_iter = self.episodes.find('.svtTab-2.playBoxBody').find('article.svtUnit')
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return len(self.episodes_iter)
+
+    def next(self):
+        if self.i == self.episodes_iter.length:
+            raise StopIteration
+
+        # Index all episodes
+        link = self.episodes_iter[self.i]
+
+        article = PyQuery(link)
+        episode = article.find('a.playLink')
+        full_url = self.crawler.baseurl + article.find('a.playLink').attr('href')
+        broadcasted = article.find('time').attr('datetime')
+
+        if len(broadcasted) < 1:
+            broadcasted = '1970-01-01 00:00:00'
+
+        #Check if the url contains an extra /Random-Title, if so, remove it
+        if len(full_url.split('/')) == 6:
+            url = full_url.rpartition('/')[0]
+        else:
+            url = full_url
+
+        if (url.find('video') != -1 or url.find('klipp') != -1) and len(broadcasted) > 1:
+
+            available = parse_date(article.attr('data-available'), '+')
+            length = article.attr('data-length')
+
+            if not episode.attr('href').startswith('http'):
+                try:
+                    article_full = PyQuery(url)
+                    thumbnail = article_full.find('img.svtHide-No-Js').eq(0).attr('data-imagename')
+                    meta = article_full.find('.playVideoMetaInfo div')
+
+                    episode = Episode()
+
+                    desc = article_full.find('.playVideoInfo p').text()
+                    if len(desc) == 0:
+                        desc = article_full.find('.playVideoInfo span')
+                    desc = sanitize_description(unicode(desc))
+
+                    if str(meta).find('Kan endast ses i Sverige') == -1:
+                        rights = 1
+                    else:
+                        rights = 2
+
+                    if str(meta).find('Kan ses i mobilen') > -1:
+                        on_device = 1
+                    else:
+                        on_device = 2
+
+                    soupEpisode = PyQuery(url)
+                    episodeTitle = soupEpisode.find('title').eq(0).text().replace('| SVT Play', '')
+                    episode.title = episodeTitle
+                    episode.title_slug = shellquote(episodeTitle)
+                    episode.http_status = 200
+                    episode.http_status_checked_date = datetime.utcnow().replace(tzinfo=utc)
+                    episode.date_available_until = available
+                    episode.date_broadcasted = broadcasted
+                    episode.length = length
+                    episode.description = desc
+                    episode.viewable_on_device = on_device
+                    episode.viewable_in = rights
+                    episode.kind_of = self.kind_of
+                    episode.thumbnail_url = thumbnail
+
+                    self.i += 1
+                    return episode
+
+                except HTTPError as err:
+                    self.i += 1
+                    return self.next()
+
+
+
+class Shows:
+    def __init__(self, crawler, url):
+        self.crawler = crawler
+        self.i = 0
+
+        self.shows = PyQuery(url)
+        self.shows_iter = self.shows.find('.playBoxBody.svtTab-1 article.svtUnit')
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.i == self.shows_iter.length:
+            raise StopIteration
+
+        # Index all shows
+        link = self.shows_iter[self.i]
+        article = PyQuery(link)
+        url = article.find('a.playLink').attr('href')
+
+        if url:
+            show_url = self.crawler.baseurl + url
+            show_title = article.text()
+            thumbnail_url = article.find('img.playGridThumbnail').attr('src').replace('medium', 'large')
+
+            if thumbnail_url.find('http') == -1:
+                thumbnail_url = self.crawler.baseurl + thumbnail_url
+
+            show = Show(title=show_title, url=show_url, thumbnail=thumbnail_url)
+
+            episodes_url = show_url + '?tab=episodes&sida=1000'
+            clip_url = show_url + '?tab=clips&sida=1000'
+
+            show.episodes = Episodes(self.crawler, episodes_url, 'episodes')
+            show.clips = Episodes(self.crawler, clip_url, 'clips')
+
+            self.i += 1
+            return show
+
+
+class Categories:
+    def __init__(self, crawler):
+        self.crawler = crawler
+        self.categories = PyQuery(self.crawler.url)
+        self.categories_iter = self.categories.find("a.playCategoryLink")
+        self.i = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.i == self.categories_iter.length:
+            raise StopIteration
+
+        link = self.categories_iter[self.i]
+
+        py_link = PyQuery(link)
+        href = py_link.attr('href')
+        html_class = href.split('/')[-1:][0]
+        title = py_link.text()
+        thumbnail_url = self.crawler.baseurl + PyQuery(link).find('img').attr('src')
+        url = self.crawler.category_url % href
+
+        category = Category(title, url, html_class, thumbnail_url)
+        category.shows = Shows(self.crawler, url)
+
+        self.i += 1
+        return category
+
+
+class SvtCrawler:
+    def __init__(self):
+        self.timezone = 'Europe/Stockholm'
         self.baseurl = 'http://www.svtplay.se'
+        self.url = 'http://www.svtplay.se/program'
+        self.category_url = 'http://www.svtplay.se%s/?tab=titles&sida=1000'
 
-    def crawl(self):
-
-        r = requests.get(self.url)
-        soup = BeautifulSoup(r.content)
-
-        for item in soup.find_all("a", text=self.searchPhrases):
-            show = requests.get(self.baseurl + item['href'] +'?tab=episodes&sida=100')
-            soupShow = BeautifulSoup(show.content)
-
-            for item2 in soupShow.select('a.playLink'):
-                print item2['href']
+        self.categories = Categories(self)
 

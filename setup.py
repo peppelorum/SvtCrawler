1d1c2a124193ee383dfc244f0de8966c4763e08f
Peppe Bergqvist <p@bergqvi.st>
2013-04-17 21:12:05 +0200

    Added setup.py to be pip-installable, closes #2

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='SVTCrawler',
+    version='0.1',
+    author='Peppe Bergqvist',
+    author_email='p@bergqvi.st',
+    packages=['svtcrawler',],
+    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='https://github.com/peppelorum/SvtCrawler/',
+    # license='LICENSE.txt',
+    description=u'SVT Crawler är en webspindel som indexerar SVT Play.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "pyquery >= 1.2",
+        "requests >= 1.2",
+        "python-dateutil >= 2.1"
+        ],
+    )
